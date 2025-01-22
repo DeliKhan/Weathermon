97ba44cb993fda35305d4442176a369be2b523d1
DeliKhan <deluckshanmurugamoorthy@gmail.com>
2025-01-22 09:25:35 -0500

    Spring Boot Initialized

---
+++ src/test/java/org/example/weathermon/WeathermonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.weathermon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeathermonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
